1e144178b2febdadf0c9fbd076cc622ae7ec49fe
Mathieu <matyou258@gmail.com>
2018-12-22 14:42:24 +0100

    V2

--- Contrôle Y.py
+++ Contrôle Y.py
@@ -1,4 +1,14 @@
 from math import*
 
-def mouvy(y, Lbras, angle):
-    rangle=radians(angle)
+def mouvy(y, Lbras):
+    alpha=degrees(asin(y/Lbras))
+    return alpha
+
+if __name__=="__main__":
+    while True:
+        y=float(input("Axe Y: "))
+        Lbras=float(input("Longueur 2eme Segment: "))
+        print("Alpha:", mouvy(y,Lbras))
+
+        continuer = input("Voulez vous continuer? [o/N]").strip().lower()
+        if not continuer.startswith('o'):
